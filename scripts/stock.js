87e6343a0b79e2b4abcc7d9455b83c13c21fa688
Leonardo Painenau <lamuraya2010@hotmail.com>
2022-12-29 04:37:50 -0300

    cargar proyecto html + javascript semi terminado

---
+++ scripts/stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: 1, nombre: "Puerta ciega de aluminio", DVH: "No lleva", cantidad: 1, desc: "Abertura hecha en modena con placas ciegas de 0,90x 2,05", precio: 120000, img: '../media/images/aberturasEcommerce/puertaCiega.jpg'},
+    {id: 2, nombre: "Puerta semi ciega de aluminio", DVH: "vidrio simple 4mm", cantidad: 1, desc: "Abertura hecha en modena con placa ciega y paño fijo de 4mm de 0,80x 2,05", precio: 150000, img: '../media/images/aberturasEcommerce/puertaSemiCiega.jpg'},
+    {id: 3, nombre: "Banderola con brazo de empuje", DVH: "4+12+4", cantidad: 1, desc: "Abertura hecha en modena con dvh 4+12+4 de 0,60x 0,60", precio: 45000, img: '../media/images/aberturasEcommerce/banderola06.jpg'},
+    {id: 4, nombre: "Banderola grande con brazo de empuje", DVH: "4+12+4", cantidad: 1, desc: "Abertura hecha en modena con dvh 4+12+4", precio: 60000, img: '../media/images/aberturasEcommerce/banderolaGrande.jpg'},
+    {id: 5, nombre: "Puerta simple tipo balcon", DVH: "3+3 +12 +6", cantidad: 1, desc: "Abertura hecha en modena con dvh esmerilado 3+3 +12 +6 de 1,20x 1,00", precio: 130000, img: '../media/images/aberturasEcommerce/puertaBalcon.jpg'},
+    {id: 6, nombre: "Ventana patagonica", DVH: "3+3 +12 +6", cantidad: 1, desc: "Abertura hecha en modena con dvh esmrilado 3+3 +12 +6", precio: 90000, img: '../media/images/aberturasEcommerce/ventanaPatagonica.jpg'},
+    {id: 7, nombre: "Ventiluz", DVH: "4+12+4", cantidad: 1, desc: "Abertura hecha en modena con dvh 4+12+4", precio: 45000, img: '../media/images/aberturasEcommerce/ventiluz.jpg'},
+    {id: 8, nombre: "Ventana de Abrir simple", DVH: "4+12+4", cantidad: 1, desc: "Abertura hecha en modena con dvh 4+12+4", precio: 65000, img: '../media/images/aberturasEcommerce/ventanaDeAbrirSimple.jpg'},
+]
