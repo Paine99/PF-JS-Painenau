ffabd28b5b41812475eb608655b066fe5cd74394
Leonardo Painenau <lamuraya2010@hotmail.com>
2023-01-17 20:12:31 -0300

    probando un fetch

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,18 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
 });
+//fetch
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'youtube-video-info.p.rapidapi.com'
+	}
+};
+
+fetch('https://youtube-video-info.p.rapidapi.com/video_formats?video=edPREMPZ5RA', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+  //fetch
+  
